ea46b3e4aa91672969fa216e0c1acee9a1c9344a
Yashi-makerr <ykesarwani41@gmail.com>
2024-12-15 11:32:01 +0530

    making travel blog website

---
+++ script.js
@@ -0,0 +1,21 @@
+// Get modal elements
+var modal = document.getElementById("imageModal");
+var mainImage = document.getElementById("mainImage");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Open modal when the main image is clicked
+mainImage.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Close modal when the user clicks the close button
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close modal when the user clicks anywhere outside the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
